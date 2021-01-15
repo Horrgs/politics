98a93e67a1447839d0b999a87a9c1d54b05531ce
Horrgs <horrgs34@gmail.com>
2021-01-15 02:27:33 -0500

    Small fixes.

--- midterms.py
+++ midterms.py
@@ -2,7 +2,6 @@ import csv
 from collections import OrderedDict
 
 results = {}
-curf_ = {}
 states = {}
 district_results = {
             'district_name': '',
@@ -14,12 +13,13 @@ district_results = {
         }
 with open('district_overall_2018.csv', encoding='utf-8', mode='r') as file:
     reader = csv.reader(file)
-
     first_line = True
+
     for row in reader:
         if first_line:
             first_line = False
             continue
+
         state_name = row[1]
         total_votes = int(row[15])
         candidate_name = row[10]
@@ -51,8 +51,10 @@ with open('district_overall_2018.csv', encoding='utf-8', mode='r') as file:
 for state in states:
     state_results = {}
     for district in states[state]:
-        curated = sorted(states[state][district]['candidates'], key=lambda t: t['vote_share'], reverse=True)
-        victor = curated[0]
+        sort_candidates = sorted(states[state][district]['candidates'], key=lambda t: t['vote_share'], reverse=True)
+        states[state][district]['candidates'] = sort_candidates
+        candidates = states[state][district]['candidates']
+        victor = candidates[0]
         district_results = {
             'district_name': states[state][district]['district_name'],
             'winner': victor['name'],
@@ -61,18 +63,16 @@ for state in states:
             'vote_share': victor['vote_share'],
             'total_votes': states[state][district]['total_votes']
         }
-        if len(curated) > 1:
-            loser = curated[1]
+        if len(candidates) > 1:
+            loser = candidates[1]
             margin = round(victor['vote_share'] - loser['vote_share'], 2)
             party = victor['party'][0].upper()
             district_results['winning_margin'] = ("{0}+{1}".format(party, margin))
 
         state_results[district] = district_results
-    cur = dict(sorted(state_results.items(), key=lambda item: item[0]))
-    results[state] = cur
+    results[state] = dict(sorted(state_results.items(), key=lambda item: item[0]))
 
 results = dict(sorted(results.items(), key=lambda item: item[0]))
-print(results)
 
 with open('2018_midterm_results.csv', encoding='utf-8', mode='w') as file:
     writer = csv.DictWriter(file, district_results.keys())
